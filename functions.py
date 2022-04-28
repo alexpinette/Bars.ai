56dabe42e03d15ef69c61a4a94f6991423310b1e
alexpinette <pinette_alex@wheatoncollege.edu>
2022-04-28 17:38:04 +0200

    added full song/artist api integration using both spotify and genius API

---
+++ functions.py
@@ -0,0 +1,140 @@
+import requests # To make get calls to API
+from bs4 import BeautifulSoup # Helps make parsing html files much easier
+
+import re, string, unicodedata
+import nltk
+import contractions
+import inflect
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import LancasterStemmer, WordNetLemmatizer
+
+# This token was granted through the use of Genius's website and allows us to make calls to the API
+client_access_token = "_mDTViAvedsTbhpMuhLml-2gkMw1DU8tKQRnqmFkQDXT2aypVvCg67wDEr4eqBIs"
+
+# Get artist object from Genius API
+def request_artist_info(artist_name, page):
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + client_access_token}
+    search_url = base_url + '/search?per_page=10&page=' + str(page)
+    data = {'q': artist_name}
+    response = requests.get(search_url, data=data, headers=headers)
+    return response
+
+# Get Genius.com song url's from artist object
+def request_song_url(artist_name, song_cap):
+    page = 1
+    songs = []
+    
+    while True:
+        response = request_artist_info(artist_name, page)
+        json = response.json()
+        
+        # Collect up to song_cap song objects from artist
+        song_info = []
+        for hit in json['response']['hits']:
+            if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
+                song_info.append(hit)
+    
+        # Collect song URL's from song objects
+        for song in song_info:
+            if (len(songs) < song_cap):
+                url = song['result']['url']
+                songs.append(url)
+            
+        if (len(songs) == song_cap):
+            break
+        else:
+            page += 1
+        
+    print('Found {} songs by {}'.format(len(songs), artist_name))
+    return songs
+
+
+def replace_contractions(text):
+    """Replace contractions in string of text"""
+    return contractions.fix(text)
+
+def remove_URL(sample):
+    """Remove URLs from a sample string"""
+    return re.sub(r"http\S+", "", sample)
+
+def remove_non_ascii(words):
+    """Remove non-ASCII characters from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        new_words.append(new_word)
+    return new_words
+
+def to_lowercase(words):
+    """Convert all characters to lowercase from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = word.lower()
+        new_words.append(new_word)
+    return new_words
+
+def remove_punctuation(words):
+    """Remove punctuation from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+def replace_numbers(words):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    p = inflect.engine()
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            new_word = p.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    return new_words
+
+def remove_stopwords(words):
+    """Remove stop words from list of tokenized words"""
+    new_words = []
+    for word in words:
+        if word not in stopwords.words('english'):
+            new_words.append(word)
+    return new_words
+
+def stem_words(words):
+    """Stem words in list of tokenized words"""
+    stemmer = LancasterStemmer()
+    stems = []
+    for word in words:
+        stem = stemmer.stem(word)
+        stems.append(stem)
+    return stems
+
+def lemmatize_verbs(words):
+    """Lemmatize verbs in list of tokenized words"""
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos='v')
+        lemmas.append(lemma)
+    return lemmas
+
+def normalize(words):
+    words = remove_non_ascii(words)
+    words = to_lowercase(words)
+    words = remove_punctuation(words)
+    words = replace_numbers(words)
+    words = remove_stopwords(words)
+    return words
+
+def preprocess(sample):
+    sample = remove_URL(sample)
+    sample = replace_contractions(sample)
+    # Tokenize
+    words = nltk.word_tokenize(sample)
+
+    # Normalize
+    return normalize(words)
